a0ed22ad20fb44167b5823ae2cd8d56cba6e5b82
Justin Balagtas <justinseebee@hotmail.com>
2024-07-08 15:15:57 -0400

    style menu

--- src/components/HashiraSelect.jsx
+++ src/components/HashiraSelect.jsx
@@ -12,37 +12,38 @@ const HashiraSelect = () => {
   return (
     <>
       <div className="hashiraMenu menuShift">
-        <button className="hashImgContainer">
+        <button className="hashImgContainer giyu">
           <img className="hashImg" src="/images/Giyu.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer gyomei">
           <img className="hashImg" src="/images/Gyomei.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer rengoku">
           <img className="hashImg" src="/images/Kyojuro.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer mit">
           <img className="hashImg" src="/images/Mitsuri.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer mui">
           <img className="hashImg" src="/images/Muichiro.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer obanai">
           <img className="hashImg" src="/images/Obanai.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer sanemi">
           <img className="hashImg" src="/images/Sanemi.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer shinobu">
           <img className="hashImg" src="/images/Shinobu.webp" alt="" />
         </button>
-        <button className="hashImgContainer">
+        <button className="hashImgContainer tengen">
           <img className="hashImg" src="/images/Tengen.webp" alt="" />
         </button>
 
         <button id="test" className="arrowButton" onClick={() => menuClose()}>
           <img src="/images/arrow.svg" alt="" />
         </button>
+
       </div>
     </>
   );
